c1d988dcafbf89313b1585e1f96bc80a2c783c3e
ariel biton <arielbiton03@gmail.com>
2024-05-19 17:25:38 +0300

    cars_data

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,38 @@
-import Image from "next/image";
+"use client"; // This is a client component 👈🏽
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import dynamic from 'next/dynamic'
+
+
+
+
 
 export default function Home() {
+
+
+  useEffect(() => {
+    console.log("useEffect");
+  }
+  , []);
+
+
+ 
+
+
+
+  
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h1>נתוני כלי רכב פעילים</h1>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <iframe title="נתוני כלי רכב פעילים"  src="https://app.powerbi.com/reportEmbed?reportId=6216abd4-8c32-411f-b231-a1e333fadaac&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <h1>על מנת לראות את הנתונים אנא התחבר לחשבון מיקרוסופט</h1>
+          
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+     
+         </main>
   );
 }
---
+++ src/components/PowerBIEmbed.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PowerBIEmbed = () => {
+  return (
+    <View>
+      <Text>PowerBIEmbed</Text>
+    </View>
+  )
+}
+
+export default PowerBIEmbed
