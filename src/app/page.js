1dc0cd1976c23869226c44507f17d54cea224e1b
ariel biton <arielbiton03@gmail.com>
2024-06-07 19:48:25 +0300

    front_update

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
+
 
 
 
@@ -27,11 +29,13 @@ export default function Home() {
 
       <h1>נתוני כלי רכב פעילים</h1>
 
-      <iframe title="נתוני כלי רכב פעילים"  src="https://app.powerbi.com/reportEmbed?reportId=6216abd4-8c32-411f-b231-a1e333fadaac&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe title="נתוני כלי רכב פעילים"  src="https://app.powerbi.com/reportEmbed?reportId=6216abd4-8c32-411f-b231-a1e333fadaac&autoAuth=true&embeddedDemo=true"  allowFullScreen="true"></iframe>
 
       <h1>על מנת לראות את הנתונים אנא התחבר לחשבון מיקרוסופט</h1>
-          
 
+      <div className={styles.footer}>
+       <h2>Designed and Developed by Jonathan Oren</h2>
+      </div>
      
          </main>
   );
